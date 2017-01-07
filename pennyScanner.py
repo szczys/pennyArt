d8f2ecf621032acdd527b5b8b1ac42be6231f7ef
Mike Szczys <szczys@hotmail.com>
2017-01-07 16:34:44 -0600

    Added green circle for penny alignment, fixed radius value use

--- pennyScanner.py
+++ pennyScanner.py
@@ -2,18 +2,18 @@ import pygame
 import pygame.camera
 from PIL import Image, ImageOps, ImageDraw
 
-DEVICE = '/dev/video1'
+DEVICE = '/dev/video0'
 SIZE = (1280, 720)
 FILENAME = 'capture.png'
 
 OffsetX = 419
 OffsetY = 110
-radius = 360
+radius = 180
 
 def makeCircleMask():
     #Tricks to make everything but penny transparent using mask:
     #http://stackoverflow.com/a/890114
-    size = (radius,radius)
+    size = (radius*2,radius*2)
     mask = Image.new('L', size, 0)
     draw = ImageDraw.Draw(mask)
     draw.ellipse((0,0)+size, fill=255)
@@ -36,17 +36,21 @@ def camstream():
     
     while capture:
         screen = camera.get_image(screen)
+        pygame.draw.circle(screen,(0,255,0),(OffsetX+radius,OffsetY+radius),radius+6,6)
         display.blit(screen, (0,0))
+
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 capture = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_s:
+                    screen = camera.get_image(screen)
+                    display.blit(screen, (0,0))
                     pygame.image.save(screen, FILENAME)
                     img = Image.open(FILENAME)
                     #Crop to Square
-                    img2 = img.crop((OffsetX, OffsetY, OffsetX + radius, OffsetY + radius))
+                    img2 = img.crop((OffsetX, OffsetY, OffsetX + (radius*2), OffsetY + (radius*2)))
                     #Do some magic to keep just a circle (the penny)
                     output = ImageOps.fit(img2, mask.size, centering=(0.5, 0.5))
                     output.putalpha(mask)
