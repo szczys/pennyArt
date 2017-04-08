7da47b11cad867b00817d123db9c49c7441d56ba
Mike Szczys <szczys@hotmail.com>
2017-04-08 15:12:43 -0500

    Beginnings of lumiosity matching

--- runner.py
+++ runner.py
@@ -2,7 +2,7 @@ import pennyArt
 
 penValues = pennyArt.unPicklePennies()
 
-mosaicVals = pennyArt.unPicklePaintByNumber()
+mosaicVals = pennyArt.unPickleHelper('circlesAndLum.p')
 #print penValues.keys()
 
 #get the nearest value in a list or set of dict keys
@@ -35,12 +35,15 @@ def map256(vals):
     return returnDict
 
 
-def placePennies(penDict,pixelList):
+def placePennies(penDict,pixelList,maxError):
     """Place pennies in the image matching best as possible
 
     Attributes:
     * penDict like dict(luminosity:[pennyFilename1,pennyFilename2])
     * pixelList like list((x,y),luminosity)
+    * maxError is how far from target luminosity is allowed. Error starts at 0
+        and increases as matching gets harder until maxError is met. Setting
+        maxError to zero enables infinite error.
 
     Returns:
     * list like list((x,y),pennyFilename,errorMargin)
@@ -66,14 +69,24 @@ def placePennies(penDict,pixelList):
     #Flow:
     ## Start with penny set
     ## set error margin
+    cantMatchFlag = False
+    errorMargin = 0
+    matchedPennyList = []
     ## Begin loop;
-    ### iterate all pennies recording matches and removing from set
-    ### record pixelLocation, pennySampleFn, error margin
-    ### increment the error margin
-    ### break if no pennies left
-    ### break if image pixels have all been filled
-    ### break if error margin too great
-    ### goto loop
+    while(cantMatchFlag == False):
+        ### iterate all pennies recording matches and removing from set
+        for pMap in pennySpread.keys():
+            closestPixMap = min(imgSpread.keys(), key=lambda x:abs(x-pMap))
+            print pMap,closestPixMap
+            ### record pixelLocation, pennySampleFn, error margin
+        ### increment the error margin
+        errorMargin += 1
+        cantMatchFlag = True
+        ### break if no pennies left
+        ### break if image pixels have all been filled
+        ### break if error margin too great
+        ### goto loop
     ## return recorded set and set of pixelLocations still remaining
+    return matchedPennyList,
 
-placePennies(penValues, mosaicVals)
+placePennies(penValues, mosaicVals, 0)
